c3cf72d723253db9bda119767d01ac2e3e506c9f
rohitsmart <rohitsmart95@gmail.com>
2024-05-31 12:10:43 +0530

    latest

--- app/Controllers/DynamicController.php
+++ app/Controllers/DynamicController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\DynamicLinkModel;
+use App\Models\UserStatusModel;
 
 class DynamicController extends BaseController
 {
@@ -14,8 +15,17 @@ class DynamicController extends BaseController
         $dynamicLinkData = $dynamicLinkModel->where('user_id', $userId)
                                             ->where('dynamic_link', $dynamicLink)
                                             ->first();
+        $userStatusModel = new UserStatusModel();
+        $userStatus = $userStatusModel->where('user_id', $userId)->first();
+        $data = [
+            'status1' => $userStatus['status_1'],
+            'status2' => $userStatus['status_2'],
+            'status3' => $userStatus['status_3'],
+            'status4' => $userStatus['status_4'],
+            'status5' => $userStatus['status_5'],
+        ];
         if ($dynamicLinkData) {
-            return view('dashboard/dynamic', ['dynamicLinkData' => $dynamicLinkData]);
+            return view('dashboard/dynamic', ['dynamicLinkData' => $dynamicLinkData, 'statuses' => $data]);
         } else {
             session()->setFlashdata('error', 'Invalid or unauthorized access to the dynamic link.');
             return redirect()->to(route_to('user-url-screen'));
